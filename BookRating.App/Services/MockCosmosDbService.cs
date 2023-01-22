6950543b34cb158e977e860a3396f147a843cae4
Marta Grzesiak <mrtgrzesiak@gmail.com>
2023-01-22 02:16:57 +0100

    Create action fix + refactor

--- BookRating.App/Services/MockCosmosDbService.cs
+++ BookRating.App/Services/MockCosmosDbService.cs
@@ -1,39 +1,39 @@
-using BookRating.App.Models;
-
-namespace BookRating.App.Services;
-
-public class MockCosmosDbService : ICosmosDbService
-{
-    private readonly BookRatingViewModel _mock = new BookRatingViewModel
-    {
-        Id = "id",
-        Description = "abcd",
-        Rating = 4,
-        Name = "film o pszczołach"
-    };
-    public Task<IEnumerable<BookRatingViewModel>> GetItemsAsync(string query)
-    {
-        return Task.FromResult(new []
-        {
-            _mock
-            
-        }.AsEnumerable());
-    }
-
-    public Task<BookRatingViewModel> GetItemAsync(string id)
-    {
-        return Task.FromResult(_mock);
-    }
-
-    public  Task AddItemAsync(BookRatingViewModel item) => Task.CompletedTask;
-
-    public async Task UpdateItemAsync(string id, BookRatingViewModel item)
-    {
-        
-    }
-
-    public async Task DeleteItemAsync(string id)
-    {
-        
-    }
-}
+// using BookRating.App.Models;
+//
+// namespace BookRating.App.Services;
+//
+// public class MockCosmosDbService : ICosmosDbService
+// {
+//     private readonly BookRatingViewModel _mock = new BookRatingViewModel
+//     {
+//         Id = "id",
+//         Description = "abcd",
+//         Rating = 4,
+//         Name = "film o pszczołach"
+//     };
+//     public Task<IEnumerable<BookRatingViewModel>> GetItemsAsync(string query)
+//     {
+//         return Task.FromResult(new []
+//         {
+//             _mock
+//             
+//         }.AsEnumerable());
+//     }
+//
+//     public Task<BookRatingViewModel> GetItemAsync(string id)
+//     {
+//         return Task.FromResult(_mock);
+//     }
+//
+//     public  Task AddItemAsync(BookRatingViewModel item) => Task.CompletedTask;
+//
+//     public async Task UpdateItemAsync(string id, BookRatingViewModel item)
+//     {
+//         
+//     }
+//
+//     public async Task DeleteItemAsync(string id)
+//     {
+//         
+//     }
+// }
